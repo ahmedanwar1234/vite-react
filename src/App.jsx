5ebf45f9c1a38f515beb416f556a256476b0c36d
ahmedanwar123 <ahmedalprenses@gmail.com>
2023-08-26 06:01:11 +0300

    vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
-
-// import Browser Router
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-//imported pages
-import About from './css/second-style/pages/about/About'
-import Home from './css/second-style/pages/Home/Home'
-
-import Contact from './css/second-style/pages/contact/Contact'
-import Gallery from './css/second-style/pages/gallery/Gallery'
-import NotFound from './css/second-style/pages/notFound/NotFound'
-import Plans from './css/second-style/pages/plans/Plans'
-import Trainers from './css/second-style/pages/trainers/Trainers'
-import Navbar from './Components-For-pages/Navbar'
-import { Footer } from './Components-For-pages/Footer'
-//end imported pages
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './css/second-style/pages/about/About';
+import Home from './css/second-style/pages/Home/Home';
+import Contact from './css/second-style/pages/contact/Contact';
+import Gallery from './css/second-style/pages/gallery/Gallery';
+import NotFound from './css/second-style/pages/notFound/NotFound';
+import Plans from './css/second-style/pages/plans/Plans';
+import Trainers from './css/second-style/pages/trainers/Trainers';
+import Navbar from './Components-For-pages/Navbar';
+import { Footer } from './Components-For-pages/Footer';
 
 function App() {
-
   return (
-    <BrowserRouter className=''>
-      <Navbar/>
-<Routes className="">
-  <Route index element={<Home/>}/>
-  <Route path='about' element={<About/>}/>
-  <Route path='gallery' element={<Gallery/>}/>
-  <Route path='contact' element={<Contact/>}/>
-  <Route path='trainers' element={<Trainers/>}/>
-  <Route path='*' element={<NotFound/>}/>
-  <Route path='plans' element={<Plans/>}/>
-
-
-</Routes>
-<Footer/>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/trainers" element={<Trainers />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
